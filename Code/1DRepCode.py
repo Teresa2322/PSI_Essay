5df939267e2817fc140dd0c6d9923eae6faec71e
Teresa2322 <teresadelaguila23@gmail.com>
2026-01-16 19:17:58 -0500

    save

--- Code/1DRepCode.py
+++ Code/1DRepCode.py
@@ -4,14 +4,22 @@ import matplotlib.pyplot as plt
 
 rng = np.random.default_rng()
 
-psi_initial = np.zeros(5)
+psi_initial = np.zeros(20)
 psi_final = np.array([])
 
-for q in psi_initial:
-	p = rng.random() #produces random number between 0 and 1
-	if p <= 0.6:
-		q = 1
-	print("bit is ", q) 
-	psi_final = np.append(psi_final, q)
+N_its = 5 #number of times to apply Error 
+p_i = 0.3 #probability of bit flip
 
-print("Qubit after one noise pass", psi_final)
+print("Initial state", psi_initial)
+
+for N_i in range(1,N_its + 1):
+	for i in range(len(psi_initial)):
+    		p = rng.random()
+    		if p <= p_i and psi_initial[i] == 0.0:
+        		psi_initial[i] = 1.0
+    		elif p <= p_i and psi_initial[i] == 1.0:
+        		psi_initial[i] = 0.0
+
+print("Qubit after",N_its," noise passes is: ", psi_initial)
+
+#some ideas: average sum over N_its? or maybe over p_i?
