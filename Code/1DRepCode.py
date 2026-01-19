969de08f791e681292ee0490c890963e8fa5dceb
Teresa2322 <teresadelaguila23@gmail.com>
2026-01-19 16:58:53 -0500

    cleaned up

--- Code/1DRepCode.py
+++ Code/1DRepCode.py
@@ -1,57 +1,45 @@
-
 import numpy as np
 import scipy as sp
-import matplotlib.pyplot as plt
 import math 
 
 rng = np.random.default_rng()
 
 psi_initial = np.zeros(8,  dtype=int )
-
-N_its = 1 #number of times to apply Error 
+ 
 p_i = 0.3 #probability of bit flip
 
-#psi_initial[0] = 1
-#psi_initial[-1] = 1
 max_decode = math.floor((len(psi_initial)-1)/2)
 
-#should maybe be defining this as functions actually 
-
-for N_i in range(1,N_its + 1):
-	for i in range(1,len(psi_initial)):
-    		p = rng.random()
-    		if p <= p_i and psi_initial[i] == 0:
-        		psi_initial[i] = 1
-    		elif p <= p_i and psi_initial[i] == 1:
-        		psi_initial[i] = 0
+for i in range(1,len(psi_initial)):
+    	p = rng.random()
+    	if p <= p_i and psi_initial[i] == 0:
+        	psi_initial[i] = 1
+    	elif p <= p_i and psi_initial[i] == 1:
+        	psi_initial[i] = 0
 
 psi_noisy = psi_initial.copy()
 
-print("Qubit after",N_its," noise passes is: ", psi_noisy)
+print("Noisy state:", psi_noisy)
 
 #syndrome calculation
 
 len_psi = len(psi_initial)
 
-syndr_arr = np.zeros( len_psi - 1, dtype = int)
-err_arr = [] # position of error (in non python index notation) on syndrome 
+syndr_arr = np.zeros(len_psi - 1, dtype = int)
+err_arr = [] # position of error on syndrome (1-indexing) 
 
-for i in range(0, len_psi - 1): #this range is fine cause indices below add 1 to the last
-	if psi_initial[i] != psi_initial[i+1]:
+for i in range(0, len_psi - 1): #0-indexing here
+	if psi_initial[i] != psi_initial[i+1]: #flag domain walls with 1
 		syndr_arr[i] = 1
 		err_arr.append(i+1)
 
-print("syndrome array is ", syndr_arr) 
-print("error position is", err_arr) 
+print("Syndrome: ", syndr_arr) 
+print("Error positions: ", err_arr) 
 
 psi_decoded = psi_noisy.copy()
 
-
-err_arr_f = err_arr.copy()
-err_arr_b = err_arr[::-1].copy() 
-
-print("Forward error array:", err_arr_f)
-print("Backward error array:", err_arr_b)
+err_arr_f = err_arr.copy() #forwards (left-right) direction
+err_arr_b = err_arr[::-1].copy() # backwards (right-left) direction
 
 def even_or_odd(n):
   if n % 2 == 0:
@@ -61,7 +49,9 @@ def even_or_odd(n):
 
 Nerr = len(err_arr)
 
-print("number of errors:", Nerr, "and max correctable is ", max_decode )
+print("Number of errors:", Nerr)
+
+#initializing some arrays needed for decoding
 
 flips_b = []
 flips_f = []
@@ -74,33 +64,29 @@ initialize_ones = np.ones(len(psi_initial),dtype = int)
 flips_ft = []
 flips_bt = []
 
-if even_or_odd(Nerr) == 1: #in case odd
-	
-	for i in range(0,Nerr - 1, 2):
+if even_or_odd(Nerr) == 1: #odd number of errors
+	for i in range(0,Nerr - 1, 2): #0-indexing, pairing up syndrome elements
 		a_f = err_arr_f[i]
 		b_f = err_arr_f[i+1]
-		flips_ft.extend(range(a_f+1, b_f+1))
+		flips_ft.extend(range(a_f+1, b_f+1)) #forwards: (a,b] position for correction
 		b_b = err_arr_b[i]
 		a_b = err_arr_b[i+1]
-		
-		flips_bt.extend(range(min(a_b,b_b)+1, max(a_b,b_b)+1))
-		#print("[",a_f,",", b_f,"]")
-		#print("[",a_b,",",b_b,"]")
-	
+		print("backwards interval: [",a_b,b_b, "]")
+		flips_bt.extend(range(min(a_b,b_b)+1, max(a_b,b_b)+1)) #backwards: same method but syndromes have been paired differently
 	for k in range(err_arr_f[-1]+1,len(psi_noisy)+1):
 		x_f.append(k)
-	for l in range(1, err_arr_b[-1]+1): #understand these ranges better, sort of reversed engineered them
+	for l in range(1, err_arr_b[-1]+1): 
 		x_b.append(l)
 	flips_ft.extend(x_f)
 	flips_bt.extend(x_b)
-	print("forward flip positions ", flips_ft)
-	print("backward flip positions ", flips_bt)	
+	print("Forward method flips suggested: ", flips_ft)
+	print("Backwards method flips suggested: ", flips_bt)	
 
 	candidate_f = np.zeros(len(psi_noisy),dtype = int)
 	candidate_b = np.zeros(len(psi_noisy),dtype = int)
 	
 	for i in flips_ft:
-		candidate_f[i-1] = 1
+		candidate_f[i-1] = 1 #adjusting back to 0-indexing
 	for i in flips_bt:
 		candidate_b[i-1] = 1
 	print("noisy candidate f", candidate_f)
@@ -108,19 +94,20 @@ if even_or_odd(Nerr) == 1: #in case odd
 	
 	weight_f = len(flips_ft)
 	weight_b = len(flips_bt)
+
 	if weight_f > weight_b:
-		psi_decoded = psi_decoded^candidate_b
+		psi_decoded = psi_decoded^candidate_b #bitwise XOR operation
 	if weight_b > weight_f:
 		psi_decoded = psi_decoded^candidate_f
 	
 	if weight_b == weight_f:
 		print("we have reached an impasse...majority vote?")
 
-if even_or_odd(Nerr) == 0: #in case even
+if even_or_odd(Nerr) == 0: #even number of errors 
 	for i in range(0,Nerr - 1, 2):
 		a_f = err_arr_f[i]
 		b_f = err_arr_f[i+1]
-		flips_f.extend(range(a_f+1, b_f+1))
+		flips_f.extend(range(a_f+1, b_f+1)) #still (a,b] intervals
 	print("flips in even case", flips_f)
 	for k in flips_f:
 		initialize_zeros[k-1] = initialize_zeros[k-1]^1
@@ -130,14 +117,13 @@ if even_or_odd(Nerr) == 0: #in case even
 
 	weight_0 = sum(initialize_zeros)
 	weight_1 = sum(initialize_ones)
+
 	if weight_0 > weight_1:
 		psi_decoded = psi_decoded^initialize_ones
 	if weight_1 > weight_0:
 		psi_decoded = psi_decoded^initialize_zeros
 	if weight_1 == weight_0:
 		print("we have reached an impasse...majority vote?")
-
-
 
 print("decoded psi", psi_decoded)
 
