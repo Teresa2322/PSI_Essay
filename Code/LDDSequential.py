9d4555f456b0bcb3aa2660df8e8e344dc3f4bdec
Teresa2322 <teresadelaguila23@gmail.com>
2026-02-19 11:11:09 -0500

    save

--- Code/LDDSequential.py
+++ Code/LDDSequential.py
@@ -3,18 +3,14 @@ import scipy as sp
 import math
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-
 rng = np.random.default_rng()
 
-p_i = 0.9 #probability of bit flip
-N_s = 30 #number of sites
-J = 1 #Hamiltonian parameter
+p_i = 0.2 #probability of bit flip
+N_s = 50 #number of sites
+J = 1 #Ising Hamiltonian parameter
 
 def Noise(psi,p_i):
-	for i in range(1,len(psi)):
-    		if rng.random() <= p_i:
-        		psi[i] = 1
-	return psi
+	return np.where(rng.random(len(psi)) <= p_i, 1, psi)
 
 psi_initial = Noise(np.zeros(N_s, dtype = int),p_i)
 
@@ -36,7 +32,8 @@ def Decode_step(i, psi, synd_arr):
 	if  i == 0:
 		DE_i = 2*J*synd_arr[0]
 	elif i == len(psi) - 1:
-		DE_i = 2*J*synd_arr[i - 1] #adjust from psi to synd indices 
+		DE_i = 2*J*synd_arr[i - 1] 
+		#adjusting from psi to synd indices 
 	else:
 		DE_i = 2*J*(synd_arr[i] + synd_arr[i-1]) 
 	if DE_i < 0:
@@ -46,7 +43,6 @@ def Decode_step(i, psi, synd_arr):
 	elif DE_i == 0:
 		if rng.random() < 0.5:
 			psi_d[i] = psi_d[i]^1 #randomly accept or reject flip
-	#RETHINK INDEXING!! syndrome is len(psi)-1
 	return psi_d
 
 def Decoding_full(psi):
@@ -54,10 +50,9 @@ def Decoding_full(psi):
 	nit = 0
 	state_hist = [psi]
 	synd_hist = [syndrome_calc(psi)]
-	#num_domain_walls = np.sum(syndrome == -1)
 	psi_d = psi.copy()
 	while (sum(psi_d) != 0 and sum(psi_d) != psi_len and nit < 10000):
-		for i in rng.permutation(psi_len): #for i in range(0, psi_len - 1):
+		for i in rng.permutation(psi_len):
 			psi_i = Decode_step(i, psi_d, syndrome_calc(psi_d))
 			psi_d = psi_i
 		state_hist.append(psi_d)
@@ -83,7 +78,7 @@ plt.gca().set_xticklabels([])
 plt.gca().set_yticklabels([])
 
 plt.ylabel("Decoding step")
-plt.title(f"State Evolution, p_i = {p_i}, N_i = {nit} ")
+plt.title(f"{N_s} state Evolution, p_i = {p_i}, N_i = {nit} ")
 
 plt.show()
 
@@ -100,6 +95,8 @@ plt.ylabel("Decoding step")
 plt.title("Syndrome Evolution During Decoding")
 '''
 
+#histogram to analyze number of iterations/scalability
+'''
 n_arr = []
 for i in range(100):
 	psi_n = Noise(np.zeros(N_s, dtype = int),p_i)
@@ -110,4 +107,4 @@ plt.title(f"Histogram: Bit Flip Probability {p_i}")
 plt.hist(n_arr, bins = 20)
 plt.xlabel("Number of Decoding Iterations")
 plt.show()
-
+'''
